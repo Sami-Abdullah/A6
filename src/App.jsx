d3d8efa893d4f0f1267620dc9109ae92862fdac1
Sami Abdullah <sami38596@gmailcom>
2026-03-30 15:02:45 +0600

    mmade navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Navbar from "./Components/Navbar"
 
 
 
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
       {/* navbar */}
-      <Navbar
+      <Navbar></Navbar>
       {/* banner */}
       {/* stats */}
       {/* 
